66da1e2f6a164a3414da48512c59f1b5f18d6f29
ynokim <40172259+ynokim@users.noreply.github.com>
2020-05-13 01:33:53 +0900

    ynokim
    main_downloader.py #0.0

--- main_code.py
+++ main_code.py
@@ -1,1 +1,17 @@
-print("I'm testing the git path! Hello GIT")
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('다음으로 로그인합니다 : {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        await message.channel.send('FROM YNOKIM 2020.05.12 TUESDAY')
+
+client.run('NTQ5MjIzNjI3Njc0MDkxNTQx.XrrPsw.waVFdtU1VcSlF229K6XPp5sSpSg')
