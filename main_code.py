3570f42f126d7e417dd45ff37994cc7961c5bcdf
ynokim <40172259+ynokim@users.noreply.github.com>
2020-06-01 16:24:13 +0900

    ynokim
    main_code.py #0.0 2020.06.01

--- main_code.py
+++ main_code.py
@@ -5,6 +5,7 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print('다음으로 로그인합니다 : {0.user}'.format(client))
+    await client.change_presence(activity=discord.Game(name='냉탕에 연어가 살거라고 믿었지'))
 
 @client.event
 async def on_message(message):
@@ -12,6 +13,6 @@ async def on_message(message):
         return
 
     if message.content.startswith('!hello'):
-        await message.channel.send('FROM YNOKIM 2020.05.12 TUESDAY')
+        await message.channel.send('FROM YNOKIM 2020.05.13 THURSDAY')
 
-client.run('token here')
+client.run('token_here')
